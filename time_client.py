14bc21f88302d687f0c6078c8c3b11ad8293371d
luopeng <814718689@qq.com>
2020-04-20 14:55:27 +0800

    修复bug:大类时间表格时间不对

--- time_client.py
+++ time_client.py
@@ -19,10 +19,13 @@ class WeekMarkDown:
 
     def add_tags(self,tags_str,duration):
         tag_list = tags_str.split(',')
-        if tag_list[-1] .find(':billable') > 0:
-            self.count += 1;
-            print("tag_list:", tag_list)
-        print(self.count)
+        if tag_list[-1] .find(':billable') > -1:
+            self.count += 1
+            if tag_list[0].find("生活") > -1:
+                print("tag_list:", tag_list)
+        # print("tag_list0:", tag_list[0])
+        # print('find_result', tag_list[0].find("生活"))
+        # print(self.count)
         first_type,second_type = tag_list[0:2]
         self.add_type_time(tags_str.strip(),duration)
         self.add_type_time(first_type.strip(),duration)
@@ -56,6 +59,8 @@ def WriteDayMarkDown():
     api_url = cfg['manic_time']['api_url']
     token = cfg['manic_time']['token']
     week_start, week_end = getWeekStartAndEnd()
+    week_start, week_end = '2020-04-04','2020-04-07'
+    print(week_start, week_end)
     url = "{}/activities?fromTime={}&toTime={}".format(api_url,week_start,week_end)
     payload = {}
     headers = {
@@ -69,6 +74,7 @@ def WriteDayMarkDown():
 
     week_md = WeekMarkDown()
     for activity in activities:
+        print(activity)
         tags_str = '{}\n'.format(activity['displayName'])
         if not week_md.check_tags(tags_str):
             print("error tags_str:", tags_str)
@@ -77,7 +83,7 @@ def WriteDayMarkDown():
         week_md.add_tags(tags_str,duration)
 
     title = ['大类', '总用时','平均每天用时']
-    day_num = 7
+    day_num = 3
     datas = [
         ['生活', "{:.1f}".format(week_md.get_type_time('生活')),"{:.1f}".format(week_md.get_type_time('生活')/day_num)],
         ['自由', "{:.1f}".format(week_md.get_type_time('自由')),"{:.1f}".format(week_md.get_type_time('自由')/day_num)],
@@ -93,57 +99,57 @@ def WriteDayMarkDown():
     f.write(time_range)
     f.write(time_table)
     f.write('\n')
-    title = [
-        '<div style="width:60px">类型</div>',
-        '<div style="text-align:center"><font color=red>预期[实际]</font></div>',
-        '<div style="width:60px;text-align:center">结果</div>',
-        '<div style="width:200px;text-align:center">总结</div>',
-    ]
+    # title = [
+    #     '<div style="width:60px">类型</div>',
+    #     '<div style="text-align:center"><font color=red>预期[实际]</font></div>',
+    #     '<div style="width:60px;text-align:center">结果</div>',
+    #     '<div style="width:200px;text-align:center">总结</div>',
+    # ]
 
-    f.write("### 工作")
-    f.write('\n')
-    work_datas = [
-        ['bug修复','状态恢复相关处理','ko','后续回顾一下笔记'],
-    ]
-    work_table = week_md.get_table(title, work_datas)
-    f.write(work_table)
-    f.write('\n')
-
-    f.write("### 业余学习")
-    f.write('\n')
-    free_datas = [
-        ['bug修复', '状态恢复相关处理', 'ko', '后续回顾一下笔记'],
-    ]
-    free_table = week_md.get_table(title, free_datas)
-    f.write(free_table)
-    f.write('\n')
-
-    f.write("### 生活")
-    f.write('\n')
-    life_datas = [
-        ['bug修复', '状态恢复相关处理', 'ko', '后续回顾一下笔记'],
-    ]
-    life_table = week_md.get_table(title, life_datas)
-    f.write(life_table)
-    f.write('\n')
-
-    f.write("### 健康")
-    f.write('\n')
-    health_datas = [
-        ['bug修复', '状态恢复相关处理', 'ko', '后续回顾一下笔记'],
-    ]
-    health_table = week_md.get_table(title, health_datas)
-    f.write(health_table)
-    f.write('\n')
-
-    f.write("### 人际交往")
-    f.write('\n')
-    communication_datas = [
-        ['bug修复', '状态恢复相关处理', 'ko', '后续回顾一下笔记'],
-    ]
-    communication_datas = week_md.get_table(title, communication_datas)
-    f.write(communication_datas)
-    f.write('\n')
+    # f.write("### 工作")
+    # f.write('\n')
+    # work_datas = [
+    #     ['bug修复','状态恢复相关处理','ko','后续回顾一下笔记'],
+    # ]
+    # work_table = week_md.get_table(title, work_datas)
+    # f.write(work_table)
+    # f.write('\n')
+    #
+    # f.write("### 业余学习")
+    # f.write('\n')
+    # free_datas = [
+    #     ['bug修复', '状态恢复相关处理', 'ko', '后续回顾一下笔记'],
+    # ]
+    # free_table = week_md.get_table(title, free_datas)
+    # f.write(free_table)
+    # f.write('\n')
+    #
+    # f.write("### 生活")
+    # f.write('\n')
+    # life_datas = [
+    #     ['bug修复', '状态恢复相关处理', 'ko', '后续回顾一下笔记'],
+    # ]
+    # life_table = week_md.get_table(title, life_datas)
+    # f.write(life_table)
+    # f.write('\n')
+    #
+    # f.write("### 健康")
+    # f.write('\n')
+    # health_datas = [
+    #     ['bug修复', '状态恢复相关处理', 'ko', '后续回顾一下笔记'],
+    # ]
+    # health_table = week_md.get_table(title, health_datas)
+    # f.write(health_table)
+    # f.write('\n')
+    #
+    # f.write("### 人际交往")
+    # f.write('\n')
+    # communication_datas = [
+    #     ['bug修复', '状态恢复相关处理', 'ko', '后续回顾一下笔记'],
+    # ]
+    # communication_datas = week_md.get_table(title, communication_datas)
+    # f.write(communication_datas)
+    # f.write('\n')
 
     f.close()
 
